b97f7b9ee99a67a19102ee5d8bfbba37385a0e34
Azat IOS <ios.azatech@Azat-MacBook-Air.local>
2017-08-20 01:00:15 +0300

    This app for fun

---
+++ iDragView/MyView.swift
@@ -0,0 +1,21 @@
+//
+//  MyView.swift
+//  iDragView
+//
+//  Created by Azat IOS on 19.08.17.
+//  Copyright © 2017 azatech. All rights reserved.
+//
+
+import UIKit
+
+class MyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
